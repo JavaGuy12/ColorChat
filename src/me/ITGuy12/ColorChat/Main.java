7d77aa163e7a7b784e21d9cb0cea8822e438af50
JavaGuy12 <itguy12games@gmail.com>
2017-11-20 17:37:56 -0500

    Initial commit

---
+++ src/me/ITGuy12/ColorChat/Main.java
@@ -0,0 +1,31 @@
+package me.ITGuy12.ColorChat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin implements Listener{
+
+	Permission colorChat = new Permission("chat.color");
+	
+	public void onEnable() {
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		pm.registerEvents(this, this);
+		pm.addPermission(colorChat);
+	}
+	
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent e) {
+		Player p = e.getPlayer();
+		if(p.hasPermission(colorChat)) {
+			e.setFormat(p.getDisplayName() + ChatColor.WHITE + ": " + ChatColor.translateAlternateColorCodes('&', e.getMessage()));
+		}
+	}
+}
